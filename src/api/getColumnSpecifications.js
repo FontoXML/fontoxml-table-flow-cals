90d304d24274497fab529792941db8afa26f4960
Martin Middel <martin.middel@liones.nl>
2015-01-16 18:13:05 +0100

    Use blueprint for computing column specifications

--- src/api/getColumnSpecifications.js
+++ src/api/getColumnSpecifications.js
@@ -28,8 +28,8 @@ define(
 					null);
 		}
 
-		function getUnitOfWidth (colSpecNode) {
-			var colWidth = colSpecNode.getAttribute('colwidth');
+		function getUnitOfWidth (colSpecNode, blueprint) {
+			var colWidth = blueprint.getAttribute(colSpecNode, 'colwidth');
 
 			if (!colWidth) {
 				colWidth = '1*'; // 1* is the implied value (https://www.oasis-open.org/specs/a502.htm)
@@ -40,9 +40,9 @@ define(
 			return unit ? unit[1] : '';
 		}
 
-		function parseColumnSpecification (columnIndex, colSpec) {
+		function parseColumnSpecification (columnIndex, colSpec, blueprint) {
 			// This is the implied value when COLWIDTH is set as null or not present.
-			var columnWidth = colSpec.getAttribute('colwidth') || '1*',
+			var columnWidth = blueprint.getAttribute(colSpec, 'colwidth') || '1*',
 				isProportion;
 
 			// @TODO: Add support for mixed values such as 121*+3px.
@@ -57,11 +57,11 @@ define(
 				isProportion = true;
 			}
 
-			var unitOfWidth = getUnitOfWidth(colSpec);
+			var unitOfWidth = getUnitOfWidth(colSpec, blueprint);
 
-			var columnSeparator = colSpec.getAttribute('colsep'),
-				rowSeparator = !!colSpec.getAttribute('rowsep') || true,
-				alignment = !!colSpec.getAttribute('align') || true;
+			var columnSeparator = blueprint.getAttribute(colSpec, 'colsep'),
+				rowSeparator = !!blueprint.getAttribute(colSpec, 'rowsep') || true,
+				alignment = !!blueprint.getAttribute(colSpec, 'align') || true;
 
 			return new ColumnSpecification(
 					alignment,
@@ -72,19 +72,20 @@ define(
 					rowSeparator,
 					unitOfWidth,
 					isProportion,
-					colSpec.getAttribute('colname'));
+					blueprint.getAttribute(colSpec, 'colname'));
 		}
 
-		return function getColumnSpecifications (tgroup) {
+		return function getColumnSpecifications (tgroup, blueprint) {
 			// COLS is a required attribute.
-			var columnCount = parseInt(tgroup.getAttribute('cols'), 10);
+			var columnCount = parseInt(blueprint.getAttribute(tgroup, 'cols'), 10);
 
 			var colspecs = [];
 
 			// Loop over the children of the TGROUP then add the COLSPECs to the array.
 			//   This is done to accomodate for nested tables.
-			for (var index = 0, length = tgroup.childNodes.length; index < length; ++index) {
-				var potentialColspec = tgroup.childNodes[index];
+			for (var potentialColspec = blueprint.getFirstChild(tgroup);
+				 potentialColspec;
+				 potentialColspec = blueprint.getNextSibling(potentialColspec)) {
 				if (domInfo.isElement(potentialColspec, 'colspec')) {
 					colspecs.push(potentialColspec);
 				}
@@ -100,13 +101,13 @@ define(
 				var columnSpecElement = colspecs[columnIndex];
 
 				if (!columnSpecElement) {
-					columnSpecifications[columnIndex] = generateColumnSpecification(columnIndex);
+					columnSpecifications[columnIndex] = generateColumnSpecification(columnIndex, blueprint);
 					continue;
 				}
 
-				if (columnSpecElement.hasAttribute('colnum')) {
-
-					var columnNumber = parseInt(columnSpecElement.getAttribute('colnum'), 10);
+				var columnNumber = blueprint.getAttribute(columnSpecElement, 'colnum');
+				if (columnNumber) {
+					columnNumber = parseInt(columnNumber, 10);
 
 					if (columnNumber < previousColnum) {
 						throw new Error('Out of order columns in a colspec are not supported');
@@ -123,7 +124,7 @@ define(
 				}
 
 				// Just parse as is
-				columnSpecifications[columnIndex] = parseColumnSpecification(columnIndex, columnSpecElement);
+				columnSpecifications[columnIndex] = parseColumnSpecification(columnIndex, columnSpecElement, blueprint);
 			}
 
 			for (var i = 0, l = columnSpecifications.length; i < l; ++i) {
