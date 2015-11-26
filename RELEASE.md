c80e42a8d36f09cb76593657a49a908fa22bbf15
Thomas Brekelmans <thomas.brekelmans@liones.nl>
2015-11-26 15:25:54 +0100

    1.0.2: Use and configure fontoxml-table-flow#2.0.0.

--- RELEASE.md
+++ RELEASE.md
@@ -1,5 +1,9 @@
 # fontoxml-table-flow-cals
 
+## 1.0.2
+
+Use and configure fontoxml-table-flow#2.0.0.
+
 ## 1.0.1
 
 Minify JSON files in build.
--- src/install.js
+++ src/install.js
@@ -1,15 +1,23 @@
 define([
 	'fontoxml-operations/operationsManager',
+	'fontoxml-table-flow/tableManager',
 
-    'json!./sx/operations.json'
+	'./calsTableStructure',
+
+	'json!./sx/operations.json'
 ], function (
 	operationsManager,
+	tableManager,
+
+	calsTableStructure,
 
 	operationsJson
 	) {
 	'use strict';
 
 	return function install () {
+		tableManager.setTableStructures([calsTableStructure]);
+
 		operationsManager.addOperations(operationsJson);
 	};
 });
