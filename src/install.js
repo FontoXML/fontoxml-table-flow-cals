bbe3a28b13dc638731e114b6c477783f36c9a353
Martin Li <martin.li@liones.com>
2019-08-05 17:11:26 +0200

    DEV-3704 Add alternatives for borders

--- src/install.js
+++ src/install.js
@@ -1,6 +1,9 @@
 import addCustomMutation from 'fontoxml-base-flow/src/addCustomMutation.js';
 import toggleCellBorder from './custom-mutations/toggleCellBorder.js';
+import operationsManager from 'fontoxml-operations/src/operationsManager.js';
 
 export default function install() {
 	addCustomMutation('calsToggleCellBorder', toggleCellBorder);
+	operationsManager.addAlternativeOperation('set-cell-border-none', 'cals-set-cell-border-none');
+	operationsManager.addAlternativeOperation('set-cell-border-all', 'cals-set-cell-border-all');
 }
