b380d4849ff25f69dd6daae30ea0019bc715b996
Martin Li <martin.li@liones.com>
2019-08-06 16:18:15 +0200

    DEV-3701 Add alternatives for aignment

--- src/install.js
+++ src/install.js
@@ -6,4 +6,33 @@ export default function install() {
 	addCustomMutation('calsToggleCellBorder', toggleCellBorder);
 	operationsManager.addAlternativeOperation('set-cell-border-none', 'cals-set-cell-border-none');
 	operationsManager.addAlternativeOperation('set-cell-border-all', 'cals-set-cell-border-all');
+
+	operationsManager.addAlternativeOperation(
+		'set-cell-horizontal-alignment-left',
+		'cals-set-cell-horizontal-alignment-left'
+	);
+	operationsManager.addAlternativeOperation(
+		'set-cell-horizontal-alignment-right',
+		'cals-set-cell-horizontal-alignment-right'
+	);
+	operationsManager.addAlternativeOperation(
+		'set-cell-horizontal-alignment-center',
+		'cals-set-cell-horizontal-alignment-center'
+	);
+	operationsManager.addAlternativeOperation(
+		'set-cell-horizontal-alignment-justify',
+		'cals-set-cell-horizontal-alignment-justify'
+	);
+	operationsManager.addAlternativeOperation(
+		'set-cell-vertical-alignment-top',
+		'cals-set-cell-vertical-alignment-top'
+	);
+	operationsManager.addAlternativeOperation(
+		'set-cell-vertical-alignment-bottom',
+		'cals-set-cell-vertical-alignment-bottom'
+	);
+	operationsManager.addAlternativeOperation(
+		'set-cell-vertical-alignment-middle',
+		'cals-set-cell-vertical-alignment-center'
+	);
 }
