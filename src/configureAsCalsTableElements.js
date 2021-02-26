ac78efe175c4dbb229878413b4596430291b4357
Stef Busking <stef.busking@gmail.com>
2021-02-26 15:08:34 +0100

    DEV-2323 Update widget area type in API docs

--- src/configureAsCalsTableElements.js
+++ src/configureAsCalsTableElements.js
@@ -340,10 +340,10 @@ import CalsTableDefinition from './table-definition/CalsTableDefinition.js';
  *
  * @param  {boolean}         [options.showInsertionWidget]               To add insertion buttons which insert a column or a row to a specific place, default false.
  * @param  {boolean}         [options.showHighlightingWidget]            To add highlighting bars which highlight columns and rows, and provide operations popover, default false.
- * @param  {Widget[]|null}   [options.columnBefore]                      To add one or multiple
+ * @param  {WidgetSubAreaByName|Widget[]|null}  [options.columnBefore]   To add one or multiple
  * widgets before each column. Column widgets are linked to the colspec elements.
  * {@link fonto-documentation/docs/editor/api/index.xml#id-9d2b1ad5-bbc1-6c44-d491-16dc213c53f2 | All widgets} are supported.
- * @param  {Widget[]|null}   [options.rowBefore]                         To add a single icon widget
+ * @param  {WidgetSubAreaByName|Widget[]|null}  [options.rowBefore]      To add a single icon widget
  * before each row by using {@link createIconWidget}. Row widgets are linked to the row elements of
  * the table. Any widget can be added but only icon widget is supported.
  * @param  {Object[]|null}   [options.columnWidgetMenuOperations]        To configure table widget menu for columns. It accepts an array of {@link ContextualOperation}s, but only supports "name" and "contents" properties. It is allowed to have only one layer of menu.
