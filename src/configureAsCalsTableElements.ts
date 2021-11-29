57db332dfa5bce0514ec70d2c2560a2db3ceaabd
wvbe <wybe@x-54.com>
2021-11-29 19:18:08 +0100

    DEV-13145 Add @fontosdk importable to every public API for which the import location should be documented

--- src/configureAsCalsTableElements.ts
+++ src/configureAsCalsTableElements.ts
@@ -280,7 +280,7 @@ import CalsTableDefinition from './table-definition/CalsTableDefinition';
  *
  * Cals tables can also be configured to be collapsible. Refer to {@link fonto-documentation/docs/configure/elements/configure-tables.xml#id-6c3f43af-b40c-4fa3-ab47-f0fd2d4ab85c our guide} to learn more.
  *
- * @fontosdk
+ * @fontosdk importable
  *
  * @category add-on/fontoxml-table-flow-cals
  *
